22b681b68407d6d5a1706840fdb094cdb51637a6
Allenyeen <allenyen511@yahoo.com.tw>
2021-11-06 15:57:17 +0800

    first commit

---
+++ app/src/main/java/com/example/shoppcar/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.shoppcar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+private RecyclerView rcv;
+private MyAdapter adapter;
+private ArrayList<String>mData=new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        for (int i =1;i<=10;i++){
+            mData.add("項目"+i);
+        }
+        rcv=(RecyclerView)findViewById(R.id.rcv);
+        rcv.setLayoutManager((new LinearLayoutManager(this)));
+        rcv.addItemDecoration((new DividerItemDecoration(this,DividerItemDecoration.VERTICAL)));
+    adapter=new MyAdapter(mData);
+        rcv.setAdapter(adapter);
+    }
+
+}
